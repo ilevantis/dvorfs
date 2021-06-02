9ed9c268b01cf439fd35f494f396549f4b3c7488
ilevantis <ilevantis@users.noreply.github.com>
2021-06-02 14:27:14 +0100

    bump version number in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'dvorfs',
-    version = '1.0.0',
+    version = '1.0.1',
     packages = find_packages(),
     python_requires='>=3.6',
     install_requires=[
